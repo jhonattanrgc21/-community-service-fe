1535797465c8394bbe60cf9c3aa763bca2e685d2
Jhonattan Garcia <jgarciac@intelix.biz>
2022-09-22 22:33:48 -0400

    Corregi un error en la peticion de la vista proyecto inscrito

--- src/app/core/protected/pages/home/services/home.service.ts
+++ src/app/core/protected/pages/home/services/home.service.ts
@@ -6,7 +6,6 @@ import { AuthService } from 'src/app/core/auth/services/auth.service';
 import { environment } from 'src/environments/environment';
 import { RegisteredProject } from '../interfaces/home.interface';
 
-
 @Injectable({
 	providedIn: 'root',
 })
@@ -23,12 +22,14 @@ export class HomeService {
 		const url: string = `${this._baseUrl}/users/get_project_info_by_student`;
 		return this._httpClient
 			.post<RegisteredProject>(url, {
-				identification:
-					'V-26000006' /*this._authService.user.identification*/,
+				identification: this._authService.user.identification,
 			})
 			.pipe(
 				tap((res: RegisteredProject) => {
-					res.date_start = this._datePipe.transform(res.date_start , 'dd/MM/yyyy')!;
+					res.date_start = this._datePipe.transform(
+						res.date_start,
+						'dd/MM/yyyy'
+					)!;
 					res.task_list.map((item) => {
 						item.date_start = this._datePipe.transform(
 							item.date_start,
@@ -41,9 +42,7 @@ export class HomeService {
 						)!;
 					});
 				}),
-				map(
-					(res: RegisteredProject) => res
-				),
+				map((res: RegisteredProject) => res),
 				catchError((err) => of({}))
 			);
 	}
