21d283ba4c0d7d402b2492a5520b42c5ca1107e4
Jhonattan Garcia <jgarciac@intelix.biz>
2022-11-15 09:12:04 -0400

    Agregue la configuracion para el despliegue a netlify

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
 	production: true,
-	baseUrlAuth: 'http://44.212.56.24/api',
-	baseUrlProjects: 'http://ec2-44-202-1-192.compute-1.amazonaws.com',
+	baseUrlAuth: 'https://facytauthapi.tk/api',
+	baseUrlProjects: 'https://serviciocomunitariofacyt.ml',
 };
